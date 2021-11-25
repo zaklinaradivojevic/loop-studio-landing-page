5c2dde259e43b6be00ff405b57aeb0f270bda1b2
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-11-25 11:17:45 +0100

    finishing mobile nav

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 function openNav() {
     document.getElementById("mobile").style.display = "block";
-    document.getElementById("header_logo").style.display = "none";
-    document.getElementById("mobile_humburger").style.display = "none";
-    document.getElementById("mobile_logo").style.display = "block";
-
-
+    document.getElementById("logo").style.display = "none";
+    document.getElementById("mobilehumburger").style.display = "none";
+    document.getElementById("mobilelogo").style.display = "block";
 }
 function closeNav() {
-    document.getElementById("header_logo").style.display = "block";
-    document.getElementById("mobile_humburger").style.display = "block";
-
+    document.getElementById("logo").style.display = "block";
+    document.getElementById("mobilehumburger").style.display = "block";
     document.getElementById("mobile").style.display = "none";
-
 }
